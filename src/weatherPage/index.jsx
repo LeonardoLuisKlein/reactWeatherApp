7d70fef78b761dc62acbcdd60ec3159104aeee00
LeonardoLuisKlein <leonardoluis.klein@outlook.com>
2022-09-01 17:24:30 -0300

    Style: padding in city title

--- src/weatherPage/index.jsx
+++ src/weatherPage/index.jsx
@@ -1,36 +1,43 @@
-import { useState } from 'react'
-import './style.scss'
+import { useState } from "react";
+import "./style.scss";
 
 export function Weather() {
-const [location, setLocation] = useState("")
-const [temp, setTemp] = useState({city: "Waiting", weather: "--", time: "Waiting", icon: ""})
+  const [location, setLocation] = useState("");
+  const [temp, setTemp] = useState({
+    city: "Waiting",
+    weather: "--",
+    time: "Waiting",
+    icon: "",
+  });
 
-function searchLocation(event){
-  if(event.key === 'Enter'){
-  fetch(`https://api.weatherapi.com/v1/current.json?key=b3972d7c329b490c9c1175956222706&q=${location}}`)
-  .then(response => response.json())
-  .then(data => {
-    setTemp ({
-      city: data.location.name,
-      weather: data.current.temp_c.toFixed(0),
-      time: data.current.condition.text,
-      icon: data.current.icon
-    })
-  })
-}
-}
+  function searchLocation(event) {
+    if (event.key === "Enter") {
+      fetch(
+        `https://api.weatherapi.com/v1/current.json?key=b3972d7c329b490c9c1175956222706&q=${location}}`
+      )
+        .then((response) => response.json())
+        .then((data) => {
+          setTemp({
+            city: data.location.name,
+            weather: data.current.temp_c.toFixed(0),
+            time: data.current.condition.text,
+            icon: data.current.icon,
+          });
+        });
+    }
+  }
 
   return (
     <div className="Weather">
-      <input 
-      type="text" 
-      placeholder='Type your location...' 
-      onChange={(event) => setLocation(event.target.value)}
-      onKeyPress={searchLocation}
+      <input
+        type="text"
+        placeholder="Type your location..."
+        onChange={(event) => setLocation(event.target.value)}
+        onKeyPress={searchLocation}
       />
       <h2>{temp.city}</h2>
       <h3>{temp.weather}°c</h3>
       <p>{temp.time}</p>
     </div>
-  )
+  );
 }
